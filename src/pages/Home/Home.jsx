93271b25db36d54e14f0ceaf6866205b391bc9cb
Modestas Skirius <mode@Modestass-Mini.lan>
2020-11-04 22:15:58 +0200

    add location and history

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,23 +2,35 @@ import React from "react";
 import { Section, Card, SearchBar } from "../../components";
 import { Products } from "../../utils/Products";
 import * as S from "./Home.style";
+import { useLocation, useHistory } from "react-router-dom";
 
 function Home() {
+  const location = useLocation();
+  const history = useHistory()
+
+  const filteredProducts = location.search
+    ? Products.filter((product) =>
+      product.title.toLowerCase().includes(location.search.slice(1).toLowerCase())
+    )
+    : Products;
+
+  console.log(location.search);
   return (
     <>
       <Section >
-        <SearchBar />
+        <SearchBar handlechange={(e) => e.key === "Enter" && history.push({ search: e.target.value})
+        } />
         <S.CardBox>
-        {Products.length === 0 && "No products found"}
-        {Products && 
-        Products.map((prod) => (
+        {filteredProducts.length === 0 && "No products found"}
+        {filteredProducts && 
+        filteredProducts.map((prod) => (
           <Card 
-          key={prod.id}
-          id={prod.id}
-          image={prod.image}
-          title={prod.title}
-          price={prod.price}
-        />
+            handlechange={(e) => history.push(`/product/${prod.id}`)}
+            key={prod.id}
+            id={prod.id}
+            image={prod.image}
+            title={prod.title}
+            price={prod.price} />
         ))
         }
         </S.CardBox>
