6630bccf43aaf6a7576d9e6fc92dab99730fd44d
Modestas Skirius <mode@Modestass-Mini.lan>
2020-11-04 22:13:42 +0200

    handlechange and onclick

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,16 +3,17 @@ import * as S from "./Card.style";
 import { CartContext } from "../../contexts/cart.context";
 import { useContext } from "react";
 
-function Card({ id, image, title, price}) {
+function Card({ id, image, title, price, handlechange}) {
   const cart = useContext(CartContext);
 
   return (
-    <S.Card onClick={() => cart.setItem(cart.item.concat([id]))} >
+    <S.Card onClick={handlechange} >
         <S.Image image={image} />   
       <S.TextBox>
         <S.Title>{title}</S.Title>
         <S.Start>Starting at</S.Start>
         <S.Price>€{price}</S.Price>  
+        <button onClick={() => cart.setItem(cart.item.concat([id]))} >Buy</button>
       </S.TextBox>
     </S.Card>
   );
