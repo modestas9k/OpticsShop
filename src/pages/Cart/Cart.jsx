5d9c66d0805b6d38a712ad9f117147f1c1eaa501
Modestas Skirius <mode@Modestass-Mini.lan>
2020-11-04 22:14:57 +0200

    add Cart

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -3,20 +3,47 @@ import { useContext } from "react";
 import { Section } from "../../components";
 import { CartContext } from "../../contexts/cart.context";
 import { Products } from "../../utils/Products";
+import * as S from "./Cart.style";
 
 function Cart() {
   const cart = useContext(CartContext);
+
+  const cartItems = cart.item.map((item, index) => ({
+    value: item,
+    quantity: cart.item.filter((x) => x === item).length,
+  }));
+
+  const uniqueItems = cartItems.filter((item, index) => 
+    cartItems.findIndex(x => x.value === item.value) === index
+  );
+
+  const productsFind = (item, index) => {
+    const produktas = Products.find((product) => product.id === item.value)
+
+    return (
+      <tr key={item.value}>
+        <td>{produktas.title}</td>
+        <td>{produktas.price}</td>
+        <td>{item.quantity}</td>
+      </tr>
+    )
+  }
+
   return (
     <Section>
+      <S.Table>
+        <thead>
+          <tr>
+            <th>Product Name</th>
+            <th>Product Price</th>
+            <th>Quantity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {uniqueItems && uniqueItems.map((item) => productsFind(item))}
+        </tbody>
+      </S.Table>
       
-      <ul>
-    {cart.item && 
-      cart.item.map((item) => (
-        <li>{Products.find((product) => product.id === item).title}</li>
-      ))
-    }
-    </ul>
-    {console.log(cart.item)}
     </Section>
   );
 }
